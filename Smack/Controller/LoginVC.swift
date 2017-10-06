e399b513dd3cdc0befe2aa5ee377c1e4d4877601
davis888 <ayos888@gmail.com>
2017-10-06 20:50:26 +0300

    Login module UI completed

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Dovvy Pacamalan on 10/6/17.
+//  Copyright © 2017 Dovvy Pacamalan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Dovvy Pacamalan on 10/6/17.
+//  Copyright © 2017 Dovvy Pacamalan. All rights reserved.
+//
+
+import Foundation
+//Seques
+let TO_LOGIN = "toLogin"
