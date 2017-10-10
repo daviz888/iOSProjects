59c3a52faa2803f7294aa8c769f97abe5d39123a
davis888 <ayos888@gmail.com>
2017-10-10 21:23:34 +0300

    fix color background with scanner

--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -29,4 +29,40 @@ class UserDataService {
     func setAvatarname(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+//    [0.113725490196078, 0.219607843137255, 0.282352941176471, 1]
+    func returnUIColor(components: String) -> UIColor {
+        
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwarapped = r else { return defaultColor }
+        guard let gUnwarapped = g else { return defaultColor }
+        guard let bUnwarapped = b else { return defaultColor }
+        guard let aUnwarapped = a else { return defaultColor }
+        
+        let rFloat = CGFloat(rUnwarapped.doubleValue)
+        let gFloat = CGFloat(gUnwarapped.doubleValue)
+        let bFloat = CGFloat(bUnwarapped.doubleValue)
+        let aFloat = CGFloat(aUnwarapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        
+        return newUIColor
+    }
+    
+    
+    
+    
 }
